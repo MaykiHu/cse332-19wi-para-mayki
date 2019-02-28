bd6b4156ff456f92d28112f7a01e3f6dd0bb32f1
M8yki <M8yki@192.168.1.24>
2019-02-27 19:05:05 -0800

    Implemented hasOver for EX11

--- src/hasOver/HasOver.java
+++ src/hasOver/HasOver.java
@@ -2,11 +2,54 @@ package hasOver;
 
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
-import cse332.exceptions.NotYetImplementedException;
 
 public class HasOver {
+	private static final ForkJoinPool POOL = new ForkJoinPool();
     public static boolean hasOver(int val, int[] arr, int sequentialCutoff) {
-        throw new NotYetImplementedException();
+        return POOL.invoke(new HasOverTask(arr, 0, arr.length, val, sequentialCutoff));
+    }
+    
+    public static boolean sequential(int[] arr, int lo, int hi, int val) {
+        for (int i = lo; i < hi; i++) {
+            if (arr[i] > val) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @SuppressWarnings("serial")
+	private static class HasOverTask extends RecursiveTask<Boolean> {
+        int[] arr;
+        int lo, hi; int val; int sequentialCutoff;
+        
+        public HasOverTask(int[] arr, int lo, int hi, int val, int sequentialCutoff) {
+            this.arr = arr;
+            this.lo = lo;
+            this.hi = hi;
+            this.val = val;
+            this.sequentialCutoff = sequentialCutoff;
+        }
+        
+        @Override
+        protected Boolean compute() {
+            if (hi - lo <= sequentialCutoff) {
+                return sequential(arr, lo, hi, val);
+            }
+
+            int mid = lo + (hi - lo) / 2;
+
+            HasOverTask left = new HasOverTask(arr, lo, mid, val, sequentialCutoff);
+            HasOverTask right = new HasOverTask(arr, mid, hi, val, sequentialCutoff);
+            
+            left.fork();
+
+            Boolean rightResult = right.compute();
+            Boolean leftResult = left.join();
+
+            return leftResult || rightResult;
+        }
+        
     }
 
     private static void usage() {
