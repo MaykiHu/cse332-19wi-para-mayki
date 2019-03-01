5fff5c389bf5fb52e5f4fee81060acfd6e0d36fd
M8yki <M8yki@192.168.1.24>
2019-02-28 22:59:31 -0800

    Attempted LongestSequence and FilterEmpty

--- src/filterEmpty/FilterEmpty.java
+++ src/filterEmpty/FilterEmpty.java
@@ -3,8 +3,8 @@ package filterEmpty;
 import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
 
-import cse332.exceptions.NotYetImplementedException;
 
 public class FilterEmpty {
     static ForkJoinPool POOL = new ForkJoinPool();
@@ -19,12 +19,86 @@ public class FilterEmpty {
     }
 
     public static int[] mapToBitSet(String[] arr) {
-        throw new NotYetImplementedException();
+        return POOL.invoke(new MapToBitTask(arr, new int[arr.length], 0, arr.length));
     }
+    
+    @SuppressWarnings("serial")
+	private static class MapToBitTask extends RecursiveTask<int[]> {
+        int[] out; String[] in;
+        int lo, hi;
+        
+        public MapToBitTask(String[] in, int[] out, int lo, int hi) {
+            this.in = in;
+            this.out = out;
+            this.lo = lo;
+            this.hi = hi;
+        }
+        
+        @Override
+        protected int[] compute() {
+            if (hi - lo <= 0) {
+                for (int i = lo; i < hi; i++) {
+                	if (in[i].length() > 0) {
+                		out[i] = 1;
+                	} // else, out[i] = 0, by default
+                }
+                return out;
+            }
 
+            int mid = lo + (hi - lo) / 2;
+
+            MapToBitTask left = new MapToBitTask(in, out, lo, mid);
+            MapToBitTask right = new MapToBitTask(in, out, mid, hi);
+            
+            left.fork();
+            right.compute();
+            left.join();
+
+            return out;
+        }
+        
+    }
     
     public static int[] mapToOutput(String[] input, int[] bitsum) {
-        throw new NotYetImplementedException();
+        POOL.invoke(new MapToOutTask(input, bitsum, new int[bitsum[bitsum.length - 1]], 0, bitsum.length));
+    }
+    
+    @SuppressWarnings("serial")
+	private static class MapToOutTask extends RecursiveTask<int[]> {
+        int[] out; String[] in; int[] bit;
+        int lo, hi;
+        
+        public MapToOutTask(String[] in, int[] bit, int[] out, int lo, int hi) {
+            this.in = in;
+            this.out = out;
+            this.lo = lo;
+            this.hi = hi;
+            this.bit = bit;
+        }
+        
+        @Override
+        protected int[] compute() {
+            if (hi - lo <= 0) {
+                for (int i = lo + 1; i < hi; i++) {
+                	if (bit[i] - bit[i - 1] > 0) {
+                		out[i] = in[i - 1].length();
+                	} 
+                }
+                return out;
+            }
+
+            int mid = lo + (hi - lo) / 2;
+
+            MapToOutTask left = new MapToOutTask(in, bit, out, lo, mid);
+            MapToOutTask right = new MapToOutTask(in, bit, out, mid, hi);
+            
+            left.fork();
+            right.compute();
+            left.join();
+
+            return out;
+        }
+        
     }
 
     private static void usage() {
--- src/longestSequence/LongestSequence.java
+++ src/longestSequence/LongestSequence.java
@@ -2,13 +2,96 @@ package longestSequence;
 
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
-import cse332.exceptions.NotYetImplementedException;
 
 public class LongestSequence {
+	private static final ForkJoinPool POOL = new ForkJoinPool();
     public static int getLongestSequence(int val, int[] arr, int sequentialCutoff) {
-        throw new NotYetImplementedException();
+    	SequenceRange seq = POOL.invoke(new LongestSequenceTask(arr, 0, arr.length, val, sequentialCutoff));
+    	return seq.longestRange;
     }
 
+    public static SequenceRange sequential(int[] arr, int lo, int hi, int val) {
+        int count = 0;
+        int longest = 0;
+        for (int i = lo; i < hi; i++) {
+            if (arr[i] == val) {
+                count++;
+            } else {
+            	longest = Math.max(longest, count);
+            	count = 0;
+            }
+        }
+        return new SequenceRange(lo, hi, longest, hi - lo);
+    }
+    
+    @SuppressWarnings("serial")
+	private static class LongestSequenceTask extends RecursiveTask<SequenceRange> {
+        int[] arr;
+        int lo, hi; int val; int sequentialCutoff;
+        
+        public LongestSequenceTask(int[] arr, int lo, int hi, int val, int sequentialCutoff) {
+            this.arr = arr;
+            this.lo = lo;
+            this.hi = hi;
+            this.val = val;
+            this.sequentialCutoff = sequentialCutoff;
+        }
+        
+        @Override
+        protected SequenceRange compute() {
+            if (hi - lo <= sequentialCutoff) {
+                return sequential(arr, lo, hi, val);
+            }
+
+            int mid = lo + (hi - lo) / 2;
+
+            LongestSequenceTask left = new LongestSequenceTask(arr, lo, mid, val, sequentialCutoff);
+            LongestSequenceTask right = new LongestSequenceTask(arr, mid, hi, val, sequentialCutoff);
+            
+            left.fork();
+
+            SequenceRange rightSeq = right.compute();
+            SequenceRange leftSeq = left.join();
+
+            if (leftSeq.matchingOnRight != rightSeq.matchingOnLeft || leftSeq.matchingOnLeft != rightSeq.matchingOnRight) { // Two sequences aren't next to each other
+            	if (leftSeq.longestRange >= rightSeq.longestRange) {
+            		return leftSeq;
+            	} else {
+            		return rightSeq;
+            	}
+            } else { // Left and right are sequences right next to each other
+            	if (arr[mid] != val) { // If the middle value of the array is not the value concerned
+            		if (leftSeq.longestRange >= rightSeq.longestRange) {
+                		return leftSeq;
+                	} else {
+                		return rightSeq;
+                	}
+            	} else { // We have to check if the sequence lengths should be added together or not
+            		int leftLongest = leftSeq.longestRange;
+            		int rightLongest = rightSeq.longestRange;
+            		int leftLen = leftSeq.sequenceLength;
+            		int rightLen = rightSeq.sequenceLength;
+            		
+            		if (leftLongest + rightLongest == leftLen + rightLen) { // If both seq only match value
+            			return new SequenceRange(lo, hi, leftLongest + rightLongest, leftLen + rightLen);
+            		} else { // Have to check if the consecutive ones in middle is greater or if left/right seq are
+            			SequenceRange totalSeq = sequential(arr, lo, hi, val);
+            			if (totalSeq.longestRange > leftLongest && totalSeq.longestRange > rightLongest) {
+            				return totalSeq;
+            			} else {
+            				if (leftSeq.longestRange >= rightSeq.longestRange) {
+                        		return leftSeq;
+                        	} else {
+                        		return rightSeq;
+                        	}
+            			}
+            		}
+            	}
+            }
+        }
+        
+    }
+    
     private static void usage() {
         System.err.println("USAGE: LongestSequence <number> <array> <sequential cutoff>");
         System.exit(2);
